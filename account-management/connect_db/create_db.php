6ca1435c2cd01e8eeef5cd1909d360a495ea1557
Jude Mailom <judemailom@gmail.com>
2013-02-01 01:26:34 +0800

    Account Management - Sign Up
    Test mic lang ng pagupload/commit.

---
+++ account-management/connect_db/create_db.php
@@ -0,0 +1,4 @@
+<?php
+	$create_db = "create database ilearn_db";
+	$query = mysql_query($create_db, $con);
+?>
---
+++ account-management/signup-process.php
@@ -0,0 +1,72 @@
+<?php
+	session_start();
+	$_SESSION = $_POST;
+	require_once "connect_db/connect.php";
+	require_once "connect_db/use_db.php";
+	
+	$flagy=0;
+	//query to check for duplicate table elements
+	$query = "select * from user";	
+	$result = mysql_query($query, $con);
+	
+	//save username
+	$uname = $_SESSION['uname'];
+	
+	//concatenate fullname & save password as md5
+	$fname = $_SESSION['fname'].' '.$_SESSION['lname'];
+	$pass = md5($_SESSION['pass1']);
+	
+		//if the table is already populated look for a possible duplicate table element
+		while ($row = mysql_fetch_assoc($result)) {
+			if($uname===$row['user_uname']){
+					echo "<section id = \"signup\">";//echo signup-module
+					echo "User credentials already exist. Try again.<br/><br/>";
+					echo "<a href = \"signup-module.php\">Sign Up</a>";
+					$flagy = 1;
+					break;
+			}
+		}
+		
+		if($flagy!=1 || mysql_num_rows($result) == 0 ){ //if there is no duplicate or table is empty, insert
+			$new_user = "insert into user (user_uname,user_password,user_fname,user_type) values(
+						'{$uname}',
+						'{$pass}',
+						'{$fname}', 
+						'{$_SESSION['type']}'
+					)";
+					$result1 = mysql_query($new_user, $con);
+					
+					if (!$result1) {
+						echo "Could not successfully run query {$new_student} from DB: " . mysql_error();
+						exit;
+					}else{
+						if('Student'===$_SESSION['type']){
+							$query1 = "select * from user where user_uname ='{$uname}'";
+							$result1 = mysql_query($query1, $con);
+							$sid =  mysql_fetch_assoc($result1);
+							$new_student = "insert into student values(
+								'{$sid['user_id']}',
+								'{$_SESSION['school']}',
+								'level'
+							)";
+							mysql_query($new_student, $con);
+						}
+						else if('Teacher'===$_SESSION['type']){
+							$query1 = "select * from user where user_uname ='{$uname}'";
+							$result1 = mysql_query($query1, $con);
+							$sid =  mysql_fetch_assoc($result1);
+							$new_teacher = "insert into teacher values(
+								'{$sid['user_id']}',
+								'dept',
+								'{$_SESSION['school']}'
+							)";
+							mysql_query($new_teacher, $con);
+						}
+						header("Location: login-module.php");
+					}
+		}
+	
+	
+
+	require_once "connect_db/close.php";
+?>
